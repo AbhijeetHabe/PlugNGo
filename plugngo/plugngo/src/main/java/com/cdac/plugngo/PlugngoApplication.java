e6169e8ffd8e4eaab4161d53e1e4ff8c8c7c43c3
AbhijeetHabe <abhijeethabe23@gmail.com>
2023-08-17 11:30:21 +0530

    adding the starter project

---
+++ plugngo/plugngo/src/main/java/com/cdac/plugngo/PlugngoApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.plugngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugngoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugngoApplication.class, args);
+	}
+
+}
---
+++ plugngo/plugngo/src/test/java/com/cdac/plugngo/PlugngoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.plugngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlugngoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
